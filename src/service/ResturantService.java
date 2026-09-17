3212174dd9bdf846895dea169cdcbd9e22e4ee2f
TerteryanDavit2012 <terteryandavit40@gmail.com>
2026-09-17 19:31:57 +0400

    fibonaci

--- src/service/ResturantService.java
+++ src/service/ResturantService.java
@@ -257,4 +257,16 @@ public class ResturantService {
             System.out.print(x[i] + " ");
              }
     }
+    static void fibonaci(int n) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
 }
